104a74b3d69c004447a5870470aea16b13e79b36
76Claudia <claudinha.gcruz@gmail.com>
2024-11-04 21:33:21 -0300

    UserDetailsService implementado

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dscommerce.dto.UserDTO;
@@ -39,23 +43,6 @@ public class UserService implements UserDetailsService {
 		return user;
 	}
 	
-	protected User authenticated() {
-		try {
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-			String username = jwtPrincipal.getClaim("username");
-			return repository.findByEmail(username).get();
-		}
-		catch (Exception e) {
-			throw new UsernameNotFoundException("Invalid user");
-		}
-	}
-	
-	@Transactional(readOnly = true)
-	public UserDTO getMe() {
-		User entity = authenticated();
-		return new UserDTO(entity);
-	}
 }
 
 
